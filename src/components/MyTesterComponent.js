ab5c77896ff7108f84b689baab0db52c263d2a45
Mahoney96 <cmahoney966@gmail.com>
2021-02-19 09:47:08 -0800

    Beginning the mapping of component elements, using styling added yesterday feb 18 21

---
+++ src/components/MyTesterComponent.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+class VirtGalOne extends React.Component{
+
+}
+
+
+// Logic Behind WHICH Type of Component to make the Gallery: //
+
+            // Criteria needed for the functionality of the VirtGalOne Component:
+            
+            // • Needs to be a larger presenational component, taking the majority of the VH of the DOM.view for the user
+
+            // • Display 20+ click-through presenatation tiles. associated with routing paths that direct users to view/buy products
+
+            // • Display (A)title, (B)image container, (C)description box, (D) MiscConatiner. 
+
+
+// Reminder of VirtGalOne Component Criteria: //
+
+            // • TBD
+
---
+++ src/components/virtGal/VirtGalOne.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+
+class VirtGalOne extends React.Component{
+    
+}
